d90a43667814cafc39db174b054c26baaa22a261
Crucialjun <nich.otieno@gmail.com>
2020-09-28 06:58:36 +0300

    Edit and delete Deals

--- app/src/main/java/com/example/travelmantics2020/DealAdapter.kt
+++ app/src/main/java/com/example/travelmantics2020/DealAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.travelmantics2020
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
@@ -51,19 +53,28 @@ class DealAdapter() : RecyclerView.Adapter<DealAdapter.DealViewHolder>() {
 
 
 
-
-
-
-    class DealViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class DealViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),View.OnClickListener {
         private val tvTitle: TextView = itemView.findViewById(R.id.tvTitle)
         private val tvDescription: TextView = itemView.findViewById(R.id.tvDescription)
         private val tvPrice: TextView = itemView.findViewById(R.id.tvPrice)
+
+        init{
+            itemView.setOnClickListener(this)
+        }
 
         fun bind(deal : TravelDeal){
             tvTitle.text = deal.title
             tvDescription.text = deal.description
             tvPrice.text = deal.price
 
+        }
+
+        override fun onClick(p0: View?) {
+            val position = adapterPosition
+            Log.d("Deal Adapter", "onClick: position = $position")
+            val selectedDeal = deals[position]
+            val action = ListFragmentDirections.actionListFragmentToInsertFragment(selectedDeal)
+            p0!!.findNavController().navigate(action)
         }
     }
 
@@ -80,4 +91,5 @@ class DealAdapter() : RecyclerView.Adapter<DealAdapter.DealViewHolder>() {
     override fun getItemCount(): Int {
         return deals.size
     }
+
 }
